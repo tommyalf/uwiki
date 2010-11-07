b6f463ba219365683750664ddbd7397c3e4959e4
tommyalf <tommyalf@gmail.com>
2010-11-07 02:34:33 +0100

    modified uwiki to respond 'pages' root path for custom pages and 'themes' path for themes. First version of themes. Change edit and show  views to perform two column layout.

--- uwiki.rb
+++ uwiki.rb
@@ -2,10 +2,15 @@ require 'sinatra'
 require 'tilt' # we need rdiscount too
 require 'erb'
 
-get '*' do
+get "/" do
+  redirect "/pages/home"
+end
+
+get '/pages/*' do
   page = params[:splat][0]
-  redirect "/home" if page == "/"
-  page_path = "#{FileUtils.pwd}/pages#{page}.md"
+  puts page
+
+  page_path = "#{FileUtils.pwd}/pages/#{page}.md"
   if File.exists? page_path
     if params.keys.include? "edit"
       action = :edit
@@ -26,14 +31,27 @@ get '*' do
   erb action, :locals => locals
 end
 
-post '*' do
+get '/themes/*' do
   page = params[:splat][0]
-  page_path = "#{FileUtils.pwd}/pages#{page}.md"
+  page_path = "#{FileUtils.pwd}/themes/#{page}"
+  if File.exists? page_path
+      action = :show
+      f = IO.read page_path
+      content = f
+  end
+  content  
+end
+
+
+
+post '/pages/*' do
+  page = params[:splat][0]
+  page_path = "#{FileUtils.pwd}/pages/#{page}.md"
   dirname = File.dirname page_path
   FileUtils.mkdir_p dirname unless File.exists? dirname
   content = params[:content]
   File.open(page_path, "w") do |file|
     file.write content
   end
-  redirect page
+  redirect "/pages/#{page}"
 end
